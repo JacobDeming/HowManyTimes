bec68e44192d3a4f58541ac871afe246a7cd1ac3
Jacob "TKC" Deming <JacobDeming91@gmail.com>
2016-11-03 17:34:45 -0400

    First commit

--- How Many Times/How Many Times.cpp
+++ How Many Times/How Many Times.cpp
@@ -2,10 +2,25 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
-
-int main()
-{
-    return 0;
+int main() {
+	int currVal = 0, val = 0;
+	if (std::cin >> currVal) {
+		int count = 1;
+		while (std::cin >> val) {
+			if (val == currVal) {
+				++count;
+			}
+			else {
+				std::cout << currVal << " occurs "
+					<< count << " times" << std::endl;
+				currVal = val;
+				count = 1;
+			}
+		}
+		std::cout << currVal << "occurs "
+			<< count << " times" << std::endl;
+	}
+	return 0;
 }
-
